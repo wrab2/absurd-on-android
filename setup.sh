7164432802935ebd36b19ed8a25eb70c89448774
wrab2 <110084383+wrab2@users.noreply.github.com>
2022-07-27 15:30:28 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ git clone https://github.com/sdrausty/termux-archlinux
 ./termux-archlinux/setupTermuxArch
 echo "alias a='./arch/startarch'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 . /data/data/com.termux/files/usr/etc/bash.bashrc
-./arch/startarch
+
