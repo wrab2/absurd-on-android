182558194576fd3a199f6f2a2c1ae759017e168a
wrab2 <110084383+wrab2@users.noreply.github.com>
2022-07-28 15:52:38 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ pkg install git
 git clone https://github.com/sdrausty/termux-archlinux
 ./termux-archlinux/setupTermuxArch
 echo "alias a='./arch/startarch'" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-. /data/data/com.termux/files/usr/etc/bash.bashrc
+source /data/data/com.termux/files/usr/etc/bash.bashrc
 
