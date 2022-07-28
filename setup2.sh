63de375f8a89273293060260d729afa18585df59
wrab2 <110084383+wrab2@users.noreply.github.com>
2022-07-28 15:46:27 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,11 +1,9 @@
 cd ./setup
-#cp ./pacman.conf /etc/pacman.conf &
 mkdir -p ~/.vnc
 cp ./passwd ~/.vnc/ &
 cp ./config ~/.vnc/ &
 mkdir -p ~/.config/i3/
 cp ./bashrc ~/.bashrc
-. ~/.bashrc
+source ./bashrc
 cd
-#pacman-key --init && 
 pacman -S i3-wm tigervnc -y && pacman -Syu
